bc4cd927b24ce2239e9e8679f14eab7a06a4f308
Helio <heliosoto17@gmail.com>
2022-02-28 16:39:38 +0100

    improve trade log

--- src/components/calculator-test/calculator-test.tsx
+++ src/components/calculator-test/calculator-test.tsx
@@ -1,5 +1,6 @@
 import { Component, h, State, Listen } from '@stencil/core';
 let array: any = []
+let arrayResult: any = []
 
 @Component({
   tag: 'calculator-box',
@@ -9,7 +10,7 @@ let array: any = []
 export class Mycomponent {
   @State() show: any = "0"
   @State() save: any;
-  @State() saveOperator: any = ""
+  @State() saveOperator: any = "="
   @State() saveNumber: number = 0
   @State() saveDisplay: number = 0
   @State() acc: string = ""
@@ -40,7 +41,11 @@ export class Mycomponent {
       this.saveNumber = parseFloat(this.save)
       this.calculate(this.saveNumber, this.saveDisplay)
       this.save = 0
-      array.push(this.show)
+      array.push(this.saveDisplay + this.saveOperator + this.saveNumber)
+      console.log(this.show)
+      arrayResult.push(this.show)
+      this.show = "0"
+      console.log(arrayResult)
 
     }
     if (ev.target.name === "reset") {
@@ -48,10 +53,12 @@ export class Mycomponent {
       this.saveNumber = 0
       this.saveDisplay = 0
       array = []
+      arrayResult = []
     }
 
     if (ev.target.value === "AC") {
       array = []
+      arrayResult = []
     }
 
   }
@@ -80,7 +87,14 @@ export class Mycomponent {
   render() {
     return <div class="calculator">
       <div class="calculator__container">
-        <div class="calculator__register">Register: {array.map(value => <span class="register__data">{value}</span>)}</div>
+        <div class="calculator__register">
+          <div class="calculator__register--box">
+            {array.map(value => <div class="register__data">{value}<span class="register__data--equal">=</span></div>)}
+          </div>
+          <div class="calculator__register--box">
+            {arrayResult.map(value => <span class="register__data">{value}</span>)}
+          </div>
+        </div>
         <div class="calculator__screen">{this.show}</div>
         <calculator-button></calculator-button>
       </div>
